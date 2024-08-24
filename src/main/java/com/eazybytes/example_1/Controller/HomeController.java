8a8d7a16fca02cbd41ef551fed621dbfccd3c090
Anushree Bhatt <anushreebhatt357@gmail.com>
2024-08-23 21:59:23 -0500

    using thymeleaf , model'

--- src/main/java/com/eazybytes/example_1/Controller/HomeController.java
+++ src/main/java/com/eazybytes/example_1/Controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.eazybytes.example_1.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"","/","/home"})
-    public String displayMessage(){
+    public String displayMessage(Model model){
+        model.addAttribute("username","Anu");
         return "home.html";
     }
 }
